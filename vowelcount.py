9dd92d53b79c1c58c2ff29d50f06f695c125ef17
2024a1r017 <2024a1r017@mietjammu.in>
2026-09-02 12:18:54 +0530

    Create vowelcount.py

---
+++ vowelcount.py
@@ -0,0 +1,4 @@
+#write a python program to take a word and count the number o vowels a,e,i,o,u
+word=input("Enter a word: ")
+count=word.count("a")+word.count("e")+word.count("i")+word.count("o")+word.count("u")
+print(count)
