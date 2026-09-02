b74880b0524f6f0fae8cb453839d8720f59506d2
2024a1r017 <2024a1r017@mietjammu.in>
2026-09-02 13:03:54 +0530

    Create evenoddindex.py

---
+++ evenoddindex.py
@@ -0,0 +1,4 @@
+#wap to take a string and separate characters present characters present at even index positions and odd index positions
+string=input("Enter a string: ")
+even=print("Characters present at even index positions:",string[1::2])
+even=print("Characters present at odd index positions:",string[::2])
