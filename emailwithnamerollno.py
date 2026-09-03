eaff2ded0f4f3f8157b1f23e48020ce9c10f90f3
2024a1r017 <2024a1r017@mietjammu.in>
2026-09-03 12:27:45 +0530

    Update emailwithnamerollno.py

--- emailwithnamerollno.py
+++ emailwithnamerollno.py
@@ -1,6 +1,6 @@
-#Take a student full name and rollno.Generate email using first 3 letters of first name,last name,rollno
-first=input("")
-last=input("")
-roll=int(input(""))
-email=first[0:3]+last[0:3]+roll[0:3]
+#Take a student full name and rollno.Generate email using first 3 letters of first name,last name,and last 3 characters of rollno
+first=input("Enter first name:")
+last=input("Enter last name:")
+roll=input("Enter rollno:")
+email=first[0:3]+last[0:3]+roll[-3:]
 print(email)
