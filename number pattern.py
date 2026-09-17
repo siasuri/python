fe7f26b51135fc7df609c195f3bd2651b6d4803e
2024a1r017 <2024a1r017@mietjammu.in>
2026-09-17 12:17:45 +0530

    Create number pattern.py

---
+++ number pattern.py
@@ -0,0 +1,10 @@
+"""wap to print the following pattern for n rows:
+1
+12
+123
+1234"""
+n=int(input("Enter the number:"))
+for i in range(1,n+1):
+  for j in range(1,i+1):
+      print(j,end=" ")
+  print()
