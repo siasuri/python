0967f177a340bb4f4745bc5caacf144bc4cad34a
2024a1r017 <2024a1r017@mietjammu.in>
2026-09-17 12:16:35 +0530

    Create floyd's triangle.py

---
+++ floyd's triangle.py
@@ -0,0 +1,14 @@
+"""wap to print floyd's triangle:
+1
+23
+456
+78910"""
+n = int(input("Enter number of rows: "))
+
+num = 1
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
