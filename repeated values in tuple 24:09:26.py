2b01657b31147cceb9841e22d90fed4e8ed2f941
2024a1r017 <2024a1r017@mietjammu.in>
2026-09-24 13:16:39 +0530

    Create repeated values in tuple 24:09:26.py

---
+++ repeated values in tuple 24:09:26.py
@@ -0,0 +1,9 @@
+#wap to store repeated values in a tuple and count how many times a given value appears
+numbers=(10,20,30,40)
+
+
+numbers = (10, 20, 30, 20, 40, 20, 50)
+
+n = int(input("Enter a value: "))
+
+print("Count =", numbers.count(n))
