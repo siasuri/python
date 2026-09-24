24e9275d43f9a58afced2251cb23e277f282da33
2024a1r017 <2024a1r017@mietjammu.in>
2026-09-24 12:09:49 +0530

    Create months in tuples. 24:09:26.py

---
+++ months in tuples. 24:09:26.py
@@ -0,0 +1,7 @@
+#wap to store all month names in a tuple.Input a month number and display its corresponding month name
+months = ("January", "February", "March", "April", "May", "June",
+          "July", "August", "September", "October", "November", "December")
+
+
+n = int(input("Enter month number: "))
+print(months[n - 1])
