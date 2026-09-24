4f6f0a83b806f55cc56092013f801d44b2c57ebd
2024a1r017 <2024a1r017@mietjammu.in>
2026-09-24 13:15:50 +0530

    Create search element 24:09:26.py

---
+++ search element 24:09:26.py
@@ -0,0 +1,7 @@
+numbers=(10,20,30,40,50)
+search=int(input("Enter number to search:"))
+for search in numbers:
+    print("Value is present")
+    print("Position:",numbers(search))
+else:
+    print("Value is present")
